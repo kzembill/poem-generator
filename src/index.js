f890925e025c98843dd6555adb6ab7297b7fad35
kzembill <kzembill@gmail.com>
2024-01-01 14:41:38 -0500

    added poem element

--- src/index.js
+++ src/index.js
@@ -16,6 +16,9 @@ function displayPoem(response) {
     let context = "You are a romatic poem expert and you love to write short poems. Your mission is to provide a simple, four-line poem in HTML form and separate each line with a <br />. Do not include a title for the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the bottom of the poem. Be sure to follow user instructions"; 
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+    let poemElement = document.querySelector("#poem");
+    poemElement.innerHTML = 'Sit tight, we are generating your poem...';
+
     console.log("Generating poem");
     console.log(`Prompt: ${prompt}`);
     console.log(`Context: ${context}`);
