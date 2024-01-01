2c4792a222ede4feb56d2fce6cc206e3d0600c34
kzembill <kzembill@gmail.com>
2024-01-01 12:47:02 -0500

    basic JS and added typewriter plugin

---
+++ src/index.js
@@ -0,0 +1,15 @@
+function generatePoem(event) {
+    event.preventDefault();
+
+    new Typewriter('#poem', {
+        strings: "dark clouds abound here"  ,
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+      });
+   
+}
+
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
